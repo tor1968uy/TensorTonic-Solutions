86960512d1bb8ba2d0a9ecba48f44e9db9add08d
GabrielR1968 <34144863+tor1968uy@users.noreply.github.com>
2026-04-01 16:32:53 -0300

    Add solution for rotate-around-z

---
+++ rotate-around-z/rotate-around-z.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def rotate_around_z(points, theta):
+    points = np.asarray(points, dtype=float)
+    
+    R = np.array([
+        [ np.cos(theta), -np.sin(theta), 0],
+        [ np.sin(theta),  np.cos(theta), 0],
+        [0,              0,              1]
+    ])
+    
+    return (R @ points.T).T
